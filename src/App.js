f4fdac9ba3101e2aa0adbdc5b6c542b51580edca
tomxkay-headway <103075248+tomxkay-headway@users.noreply.github.com>
2022-04-05 23:18:34 -0500

    set up mock data create mock data to implement ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import "./styles.css";
+// import { useQuery, gql } from "@apollo/client";
+
+// const GET_ALL_LINKS = gql`
+//   query GetAllLinks {
+//     allLinks {
+//       id
+//       url
+//       slug
+//     }
+//   }
+// `;
+
+const data = {
+  allLinks: [
+    {
+      id: 1,
+      url: "test url 1",
+      slug: "test slug 1"
+    },
+    {
+      id: 2,
+      url: "test url 2",
+      slug: "test slug 2"
+    }
+  ]
+};
 
 export default function App() {
+  // const { loading, error, data } = useQuery(GET_ALL_LINKS);
+
+  console.log({ data });
+
+  // if (loading) return <p>loading...</p>;
+  // if (error) return <p>error</p>;
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {data.allLinks.map((link) => {
+        return <div key={link.id}>{link.url}</div>;
+      })}
     </div>
   );
 }
