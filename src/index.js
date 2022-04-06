471a14bde60ddaf379c49cd9ebcb568bc33e845b
tomxkay-headway <103075248+tomxkay-headway@users.noreply.github.com>
2022-04-05 22:52:18 -0500

    config apollo client configure apollo client and provider to access apollo hooks for data query and manipulation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://6wz08u.sse.codesandbox.io",
+  cache: new InMemoryCache()
+});
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
